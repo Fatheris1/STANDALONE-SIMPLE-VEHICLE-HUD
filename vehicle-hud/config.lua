21f8dadd19b2042668d76c5d7719e49ec0dc04f2
Fatheris <123958337+Fatheris1@users.noreply.github.com>
2024-12-18 12:28:04 +0200

    Update config.lua

--- vehicle-hud/config.lua
+++ vehicle-hud/config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.SpeedUnit = 'mph'  -- Use 'kmh' for kilometers or 'mph' for miles
+Config.SpeedUnit = 'kmh'  -- Use 'kmh' for kilometers or 'mph' for miles
 Config.HudPosition = 'right'  -- Use 'right' for default position or 'left' for position near the map
